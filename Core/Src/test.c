5c83a02772be82761c1c81eab26e388809e0dfb5
iypeep <401219981@qq.com>
2024-09-13 23:04:36 +0800

    添加标准外设库（Standard Peripheral Library）库

--- Core/Src/test.c
+++ Core/Src/test.c
@@ -1,10 +1,8 @@
 #include "stm32f103xb.h"
 
 int main(void) {
-   RCC->APB2ENR = 0x00000010;
-   GPIOC->CRH = 0x00300000;
-   GPIOC->ODR = 0x00000000;
-   while (1) {
+   __HAL_RCC_GPIOA_CLK_ENABLE()
+      while (1) {
 
-   }
+      }
 }
