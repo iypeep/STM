9409583f011a1c4783606f39f7f670f9b8e70c49
iypeep <401219981@qq.com>
2024-09-23 22:47:39 +0800

    OLED屏

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,26 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
-#include "Buzzer.h"
-#include "LightSensor.h"
+#include "OLED.h"
 
 int main(void) {
-	Buzzer_Init();			//蜂鸣器初始化
-	LightSensor_Init();		//光敏传感器初始化
+	/*模块初始化*/
+	OLED_Init();		//OLED初始化
+
+	/*OLED显示*/
+	OLED_ShowChar(1, 1, 'A');				//1行1列显示字符A
+
+	OLED_ShowString(1, 3, "HelloWorld!");	//1行3列显示字符串HelloWorld!
+
+	OLED_ShowNum(2, 1, 12345, 5);			//2行1列显示十进制数字12345，长度为5
+
+	OLED_ShowSignedNum(2, 7, -66, 2);		//2行7列显示有符号十进制数字-66，长度为2
+
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);		//3行1列显示十六进制数字0xA5A5，长度为4
+
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);		//4行1列显示二进制数字0xA5A5，长度为16
+	//C语言无法直接写出二进制数字，故需要用十六进制表示
 
 	while (1) {
-		if (LightSensor_Get() == 1)		//如果当前光敏输出1
-		{
-			Buzzer_ON();				//蜂鸣器开启
-		}
-		else							//否则
-		{
-			Buzzer_OFF();				//蜂鸣器关闭
-		}
+
 	}
 }
